cbabce456d3453498eb29f216915cd1ba673f667
rapha94 <raphael.presberg@gmail.com>
2017-12-07 17:20:21 +0100

    ajout discirminent analysis

---
+++ discriminent_analysis_basis.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import load_iris
+
+from sklearn.datasets import make_blobs
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+import numpy as np
+
+iris = load_iris()
+
+test_idx = [0, 10, 20, 30, 40, 50, 100, 120, 130, 145, 146]
+
+#training data
+
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis = 0)
+
+
+# testing data
+
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+clf = LinearDiscriminantAnalysis()
+clf = clf.fit(train_data, train_target)
+
+
+print (test_target)
+print (clf.predict(test_data))
+
+
